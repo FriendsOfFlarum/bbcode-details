bbecd7405b463e2676cf555139e30327f76f55e5
David Sevilla Martín <dsevilla192@icloud.com>
2019-02-06 19:50:44 -0500

    Update for Flarum beta 8

--- bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Contracts\Events\Dispatcher;
-use Sijad\Details\Listener;
-
-return function (Dispatcher $events) {
-    $events->subscribe(Listener\AddClientAssets::class);
-    $events->subscribe(Listener\AddBBCode::class);
-};
